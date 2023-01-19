f4d11790b63bce0840514abce72a3b25b62be5ac
naonom <naonoma@icloud.com>
2023-01-19 15:59:20 +0900

    add ageBill

--- src/main/java/com/example/demo/JudgeUtils.java
+++ src/main/java/com/example/demo/JudgeUtils.java
@@ -7,6 +7,8 @@ public class JudgeUtils extends MiraiPrjApplication {
 
 	// 定数
 	public static final int AGE = 17;
+	
+	public static final int AGEBill = 18;
 
 	/**
 	 * 課金対象か判定するメソッド<br>
@@ -26,7 +28,7 @@ public class JudgeUtils extends MiraiPrjApplication {
 		// 現在の年月日のみをセット
 		compareCal.set(curYear, curMonth, curDate);
 		// 年からプレイ可能年齢を引く
-		compareCal.add(Calendar.YEAR, -AGE);
+		compareCal.add(Calendar.YEAR, -AGEBill);
 
 		// 生年月日を設定
 		Calendar birthCal = Calendar.getInstance();
